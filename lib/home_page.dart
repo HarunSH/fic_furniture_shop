6093d6cd0b0d77376775e1ab921e18ba371c5eeb
Surya <suryaorenz@gmail.com>
2022-11-14 15:07:09 +0700

    update

---
+++ lib/home_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
