c9eba9ab268c4d03eb18d4b04501dcadc20de767
Surya <suryaorenz@gmail.com>
2022-11-16 17:26:32 +0700

    udapte

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:fic_furniture_shop/detail_page.dart';
 import 'package:fic_furniture_shop/model/product.dart';
 import 'package:flutter/material.dart';
 
@@ -14,6 +15,20 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.grey[700],
+        unselectedItemColor: Colors.grey[400],
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_basket), label: 'Keranjang'),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Favorite'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+      ),
       body: ListView(
         children: [
           const SizedBox(height: 16),
@@ -24,7 +39,7 @@ class _HomePageState extends State<HomePage> {
           categories(),
           const SizedBox(height: 20),
           recommended(),
-          const SizedBox(height: 20),
+          const SizedBox(height: 10),
           gridView(),
         ],
       ),
@@ -38,7 +53,7 @@ class _HomePageState extends State<HomePage> {
       child: Row(
         children: [
           Material(
-            color: Colors.grey[200],
+            color: Colors.grey[300],
             borderRadius: BorderRadius.circular(8),
             child: InkWell(
               onTap: () {},
@@ -128,26 +143,74 @@ class _HomePageState extends State<HomePage> {
           mainAxisExtent: 240),
       itemBuilder: (context, index) {
         Product product = dummyProduct[index];
-        return Container(
-          height: 240,
-          decoration: BoxDecoration(
-            color: Colors.grey,
-            borderRadius: BorderRadius.circular(30),
-          ),
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Center(
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(120),
-                child: Image.asset(
-                  product.image,
-                  width: 120,
-                  height: 120,
-                  fit: BoxFit.cover,
-                ),
-              ),
+        return GestureDetector(
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return DetailPage(product: product);
+            }));
+          },
+          child: Container(
+            height: 240,
+            decoration: BoxDecoration(
+              color: Colors.grey[100],
+              borderRadius: BorderRadius.circular(20),
             ),
-          ]),
+            child: Stack(
+              children: [
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                  const SizedBox(height: 12),
+                  Center(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(120),
+                      child: Image.asset(
+                        product.image,
+                        width: 140,
+                        height: 140,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Text(
+                      product.name,
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 12),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 12),
+                      child: Row(
+                        children: [
+                          Text(
+                            '\$${product.price}',
+                            style: TextStyle(
+                                color: Colors.grey[500], fontSize: 18),
+                          ),
+                          const Spacer(),
+                          const Icon(Icons.star, color: Colors.amber, size: 16),
+                          const SizedBox(width: 4),
+                          Text(product.rate.toString(),
+                              style: TextStyle(
+                                  color: Colors.grey[500], fontSize: 14))
+                        ],
+                      )),
+                ]),
+                Positioned(
+                  top: 8,
+                  left: 8,
+                  child: Icon(
+                    Icons.favorite_border,
+                    color: Colors.red,
+                  ),
+                )
+              ],
+            ),
+          ),
         );
       },
     );
@@ -164,7 +227,9 @@ Widget recommended() {
         Text(
           'Recommended Furnitures',
           style: TextStyle(
-              color: Colors.black, fontWeight: FontWeight.bold, fontSize: 14),
+              color: Colors.blueAccent,
+              fontWeight: FontWeight.bold,
+              fontSize: 14),
         ),
       ],
     ),
